406e8d5b20556ac9f4fa75c8191310c6693921a7
Asatryan0700 <121944499+Asatryan0700@users.noreply.github.com>
2023-01-09 22:17:17 +0300

    Update template.js

--- comonents/template.js
+++ comonents/template.js
@@ -83,6 +83,22 @@ export default {
                 height: 350px;
                 background-color: yellow;
             }
+            .hair1{
+                display: block;
+                position: absolute;
+            }
+            .hair2{
+                display: none;
+                position: absolute;
+            }
+            .hair3{
+                display: none;
+                position: absolute;
+            }
+            .hair4{
+                display: none;
+                position: absolute;
+            }
             .avatar-body{
                 display: block;
                 position: absolute;
@@ -91,7 +107,66 @@ export default {
                 display: none;
                 position: absolute;
             }
-             .btn-body1{
+            .avatar-body3{
+                display: none;
+                position: absolute;
+            }
+            .avatar-body4{
+                display: none;
+                position: absolute;
+            }
+            .avatar-head{
+                display: block;
+                position: absolute;
+            }
+            .eye{
+                display: block;
+                position: absolute;
+            }
+            .eye2{
+                display: none;
+                position: absolute;
+            }
+            .eye3{
+                display: none;
+                position: absolute;
+            }
+            .month{
+                display: block;
+                position: absolute;
+            }
+            .old_month{
+                display: none;
+                position: absolute;
+            }
+            .var_body, .var_hair, .var_eyes, .var_month{
+                width: 160px;
+                height: 400px;
+                padding: 50px;
+                
+            }
+            .btn-hair{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/hair.png") center / cover no-repeat;
+            }
+            .btn-hair2{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/hair2.png") center / cover no-repeat;
+            }
+            .btn-hair3{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/hair3.png") center / cover no-repeat;
+            }
+            .btn-body1{
                 width: 50px;
                 border: none;
                 cursor: pointer;
@@ -104,6 +179,59 @@ export default {
                 cursor: pointer;
                 height: 64px;
                 background: url("./assets/body2.png") center / cover no-repeat;
+            }
+            .btn-body3{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/body3.png") center / cover no-repeat;
+            }
+            .btn-body4{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/body4.png") center / cover no-repeat;
+            }
+            .btn-eyes{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: black url("./assets/eye_green.png") center / cover no-repeat;
+            }
+            .btn-eyes2{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: black url("./assets/eye_blue.png") center / cover no-repeat;
+            }
+            .btn-eyes3{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: black url("./assets/eye_hb.png") center / cover no-repeat;
+            }
+            .btn-month{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/rot.png") center / cover no-repeat;
+            }
+            .btn-month2{
+                width: 50px;
+                border: none;
+                cursor: pointer;
+                height: 64px;
+                background: url("./assets/old_rot.png") center / cover no-repeat;
+            }
+            .variants{
+                display: grid;
+                grid-template-columns: 1fr 1fr; 
             }
             </style>
         `
