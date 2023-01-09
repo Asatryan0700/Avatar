7be1641f16ff63592c22c4b62054f1adc7cf48cc
Asatryan0700 <121944499+Asatryan0700@users.noreply.github.com>
2023-01-09 22:05:11 +0300

    Update form.js

--- comonents/form.js
+++ comonents/form.js
@@ -11,42 +11,13 @@ export default class CoolForm extends HTMLElement {
         this.innerHTML = template.render({bg: this.getAttribute("bg")});
         const btnBody = document.querySelector(".btn-body1");
         const btnBody2 = document.querySelector(".btn-body2");
-        const btnBody3 = document.querySelector(".btn-body3");
-        const btnBody4 = document.querySelector(".btn-body4");
 
         btnBody.addEventListener("click", this.AddBody1);
         btnBody2.addEventListener("click", this.AddBody2);
-        btnBody3.addEventListener("click", this.AddBody3);
-        btnBody4.addEventListener("click", this.AddBody4);
-
-        const btnHair = document.querySelector(".btn-hair");
-        const btnHair2 = document.querySelector(".btn-hair2");
-        const btnHair3 = document.querySelector(".btn-hair3");
-        btnHair.addEventListener("click", this.AddHair1);
-        btnHair2.addEventListener("click", this.AddHair2);
-        btnHair3.addEventListener("click", this.AddHair3);
-
-        const btnEyes = document.querySelector(".btn-eyes");
-        const btnEyes2 = document.querySelector(".btn-eyes2");
-        const btnEyes3 = document.querySelector(".btn-eyes3");
-
-        btnEyes.addEventListener("click", this.AddEyes1);
-        btnEyes2.addEventListener("click", this.AddEyes2);
-        btnEyes3.addEventListener("click", this.AddEyes3);
-
-        const btnMonth = document.querySelector(".btn-month");
-        const btnMonth2 = document.querySelector(".btn-month2");
-
-        btnMonth.addEventListener("click", this.addMonth1);
-        btnMonth2.addEventListener("click", this.addMonth2);
-
-
 
     }
    
     AddBody1(){
-        const addBody3 = document.querySelector(".avatar-body3");
-        const addBody4 = document.querySelector(".avatar-body4");
         const addBody2 = document.querySelector(".avatar-body2");
         const addBody = document.querySelector(".avatar-body");
 
@@ -64,8 +35,6 @@ export default class CoolForm extends HTMLElement {
         }
     }
     AddBody2(){
-        const addBody3 = document.querySelector(".avatar-body3");
-        const addBody4 = document.querySelector(".avatar-body4");
         const addBody2 = document.querySelector(".avatar-body2");
         const addBody = document.querySelector(".avatar-body");
 
