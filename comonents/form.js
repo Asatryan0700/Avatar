b20374bf827b772f118c6c21dfc813ddff8c227b
Asatryan0700 <121944499+Asatryan0700@users.noreply.github.com>
2023-01-09 22:05:45 +0300

    Update form.js

--- comonents/form.js
+++ comonents/form.js
@@ -8,7 +8,7 @@ export default class CoolForm extends HTMLElement {
         // this.data = {
         //     type: this.type
         // }
-        this.innerHTML = template.render({bg: this.getAttribute("bg")});
+        this.innerHTML = template.render({ bg: this.getAttribute("bg") });
         const btnBody = document.querySelector(".btn-body1");
         const btnBody2 = document.querySelector(".btn-body2");
 
@@ -17,39 +17,38 @@ export default class CoolForm extends HTMLElement {
 
     }
    
-    AddBody1(){
+    AddBody1() {
         const addBody2 = document.querySelector(".avatar-body2");
         const addBody = document.querySelector(".avatar-body");
 
-        if(addBody.style.display === "none"){
+        if (addBody.style.display === "none") {
             addBody.style.display = "block";
             addBody2.style.display = "none";
             addBody3.style.display = "none";
             addBody4.style.display = "none";
            
-        } else{
+        } else {
             addBody.style.display = "block";
             addBody4.style.display = "none";
             addBody3.style.display = "none";
             addBody2.style.display = "none";
         }
     }
-    AddBody2(){
+    AddBody2() {
         const addBody2 = document.querySelector(".avatar-body2");
         const addBody = document.querySelector(".avatar-body");
 
-        if(addBody2.style.display === "none"){
+        if (addBody2.style.display === "none") {
             addBody.style.display = "none";
             addBody2.style.display = "block";
             addBody3.style.display = "none";
             addBody4.style.display = "none";
            
-        } else{
+        } else {
             addBody.style.display = "block";
             addBody4.style.display = "none";
             addBody3.style.display = "none";
             addBody2.style.display = "none";
         }
     }
-  
- 
+}
