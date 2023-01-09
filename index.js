d35a1c0838fbb30dc0b5cd5817be71d16365fb9f
Asatryan0700 <121944499+Asatryan0700@users.noreply.github.com>
2023-01-09 21:53:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import CoolForm from "./components/form.js";
+import CoolForm from "./comonents/form.js";
 
 if (!customElements.get('cool-form')) {
     customElements.define('cool-form', CoolForm);
